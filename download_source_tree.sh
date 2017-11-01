568f078c673eae1ea9c6293e9d1276d619c555c5
Matt Delco <delco@google.com>
2017-10-31 21:01:56 -0700

    manifest repo

--- download_source_tree.sh
+++ download_source_tree.sh
@@ -6,18 +6,21 @@ set -o pipefail
 set -x
 
 MANIFEST_REPO_URL=""
+MANIFEST_BRANCH="master"
 MANIFEST_FILE="default.xml"
 
 function usage() {
   echo "$0
-    -m <file>  name of manifest in repo (optional, defaults to default.xml)
-    -u <url>   url for git repo containing manifest"
+    -b <branch>  branch to use in manifest (optional, defaults to master)
+    -m <file>    name of manifest in repo (optional, defaults to default.xml)
+    -u <url>     url for git repo containing manifest"
   exit 1
 }
 
-while getopts m:u: arg ; do
+while getopts b:m:u: arg ; do
   case "${arg}" in
-    m) MANIFEST_FILE="${MANIFEST_FILE}";;
+    b) MANIFEST_BRANCH="${OPTARG}"
+    m) MANIFEST_FILE="${OPTARG}";;
     u) MANIFEST_REPO_URL="${OPTARG}";;
     *) usage;;
   esac
@@ -25,5 +28,5 @@ done
 
 [[ -z "${MANIFEST_REPO_URL}" ]] && usage
 
-./repo init -u "${MANIFEST_REPO_URL}" -m "${MANIFEST_FILE}"
+./repo init -u "${MANIFEST_REPO_URL}" -m "${MANIFEST_FILE}" -b "${MANIFEST_BRANCH}"
 ./repo sync
